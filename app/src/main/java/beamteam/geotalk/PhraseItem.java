399347e4acf6c950bfc00f8c0dfd217f8d0476f7
David Nahm <davidsnahm@berkeley.edu>
2018-11-21 20:11:39 -0800

    implemented saved phrases screen

--- app/src/main/java/beamteam/geotalk/PhraseItem.java
+++ app/src/main/java/beamteam/geotalk/PhraseItem.java
@@ -2,6 +2,7 @@ package beamteam.geotalk;
 
 public class PhraseItem {
     private String phraseText;
+    private boolean saved = false;
     public PhraseItem(String phraseText) {
         this.phraseText = phraseText;
     }
