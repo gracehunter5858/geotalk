5da0fcf42fb73731c5f60613c0d54eb7d9c29c93
David Nahm <davidsnahm@berkeley.edu>
2018-12-02 20:40:46 -0800

    implemented for general fragment

--- app/src/main/java/beamteam/geotalk/SavedFragment.java
+++ app/src/main/java/beamteam/geotalk/SavedFragment.java
@@ -4,22 +4,29 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import beamteam.geotalk.db.AppDatabase;
+import beamteam.geotalk.db.SavedPhrase;
 import beamteam.geotalk.db.SavedPhraseDAO;
 
 
 public class SavedFragment extends Fragment {
     private SavedPhraseDAO savedPhraseDAO;
     private SavedPhrasesRecyclerAdapter adapter;
+    private ArrayList<String> savedPhrases;
+    private ArrayList<String> targetPhrases;
 
     public SavedFragment() {
-
+        savedPhrases = new ArrayList<String>();
+        targetPhrases = new ArrayList<String>();
     }
 
     public static SavedFragment newInstance() {
@@ -31,16 +38,49 @@ public class SavedFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        savedPhraseDAO = AppDatabase.getInMemoryDatabase(this.getContext()).getSavedPhraseDAO();
-        List<PhraseTuple> phrases = savedPhraseDAO.getSavedPhrases(1, 1);
-        System.out.println(phrases.get(0).source);
-        System.out.println(phrases.get(0).target);
+
     }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        savedPhraseDAO = AppDatabase.getInMemoryDatabase(this.getContext()).getSavedPhraseDAO();
+        List<PhraseTuple> phrases = savedPhraseDAO.getSavedPhrases(1, 1);
+
+        View view = inflater.inflate(R.layout.fragment_saved, container, false);
+
+        for (int i = 0; i < phrases.size(); i++) {
+            savedPhrases.add(phrases.get(i).source);
+            targetPhrases.add(phrases.get(i).target);
+        }
+        initSavedPhraseRecyclerView(savedPhrases, targetPhrases, view);
+
+        ItemTouchHelper.SimpleCallback simpleItemTouchCallback = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT | ItemTouchHelper.DOWN | ItemTouchHelper.UP) {
+
+            @Override
+            public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+                Toast.makeText(SavedFragment.this.getContext(), "on Move", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+
+            @Override
+            public void onSwiped(RecyclerView.ViewHolder viewHolder, int swipeDir) {
+                Toast.makeText(SavedFragment.this.getContext(), "Removed Phrase", Toast.LENGTH_SHORT).show();
+                //Remove swiped item from list and notify the RecyclerView
+                int position = viewHolder.getAdapterPosition();
+                savedPhraseDAO.delete(new SavedPhrase(1, 1, 1, savedPhrases.get(position), targetPhrases.get(position)));
+                savedPhrases.remove(position);
+                targetPhrases.remove(position);
+                adapter.notifyDataSetChanged();
+
+            }
+        };
+
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(simpleItemTouchCallback);
+        itemTouchHelper.attachToRecyclerView((RecyclerView) view.findViewById(R.id.RECYCLERVIEW_SAVED));
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_saved, container, false);
+
+        return view;
     }
 
 
--- app/src/main/java/beamteam/geotalk/db/SavedPhrase.java
+++ app/src/main/java/beamteam/geotalk/db/SavedPhrase.java
@@ -3,14 +3,15 @@ package beamteam.geotalk.db;
 import android.arch.persistence.room.Entity;
 import android.support.annotation.NonNull;
 
-@Entity (tableName = "saved_phrases", primaryKeys = {"userID", "phraseID", "language"})
+@Entity (tableName = "saved_phrases", primaryKeys = {"source", "target"})
 public class SavedPhrase {
 
     public int userID;
     public int phraseID;
-    @NonNull
     public int language;
+    @NonNull
     public String source;
+    @NonNull
     public String target;
 
     public SavedPhrase(int userID, int phraseID, int language, String source, String target) {
