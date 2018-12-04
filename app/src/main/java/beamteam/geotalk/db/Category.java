359d56b5829b74b5c8b86ea4f6b121091a5b452a
Grace Hunter <gracehunter@calvisitor-10-105-136-67.calvisitor.1918.berkeley.edu>
2018-12-03 20:26:20 -0800

    add airport and general categories

--- app/src/main/java/beamteam/geotalk/db/Category.java
+++ app/src/main/java/beamteam/geotalk/db/Category.java
@@ -24,7 +24,23 @@ public class Category {
 
     public static Category[] getInitialData() {
         return new Category[] {
-                new Category(0, "airport", "baggage"),
+                // Locations
+                new Category(0, "airport", "Baggage"),
+                new Category( 1, "airport", "On the Plane"),
+                new Category(3, "airport", "Customs"),
+                new Category(4, "airport", "Transportation"),
+                new Category(5, "airport", "Getting to the Plane"),
+                new Category(6, "airport", "Problems"),
+
+                // General
+                new Category(0, "general", "Basics"),
+                new Category( 1, "general", "Numbers"),
+                new Category(3, "general", "Dates & Times"),
+                new Category(4, "general", "Directions"),
+                new Category(5, "general", "Food"),
+                new Category(6, "general", "Money"),
+                new Category(6, "general", "Problems"),
+                new Category(6, "general", "Questions"),
         };
     }
 
